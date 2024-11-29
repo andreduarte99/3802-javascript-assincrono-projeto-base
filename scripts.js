514e1b76d80885b371ea677da8646aba3c0bad70
André Duarte <140335@corp.hrln.br>
2024-11-29 17:45:02 -0300

    finalizando etapa 2 do curso

---
+++ scripts.js
@@ -0,0 +1,40 @@
+const uploadBtn = document.getElementById("upload-btn");
+const inputUpload= document.getElementById("image-upload");
+
+uploadBtn.addEventListener("click", () => {
+    inputUpload.click();
+})
+
+function lerConteudoDoArquivo(arquivo){
+    return new Promise((resolve, reject) => {
+        const leitor = new FileReader();
+        leitor.onload = () => {
+            resolve({url: leitor.result, nome: arquivo.name});
+        }
+
+        leitor.onerror = () => {
+            reject(`Erro na leitura do arquivo ${arquivo.name}`);
+        }
+
+        leitor.readAsDataURL(arquivo);
+    });
+}
+
+
+
+const imagemPrincipal = document.querySelector(".main-imagem");
+const nomeDaImagem = document.querySelector(".container-imagem-nome p")
+
+inputUpload.addEventListener("change", async (evento) => {
+    const arquivo = evento.target.files[0];
+
+    if(arquivo){
+        try{
+            const conteudoDoArquivo = await lerConteudoDoArquivo(arquivo);
+            console.log(imagemPrincipal.src = conteudoDoArquivo.url);
+            console.log(nomeDaImagem.textContent = conteudoDoArquivo.nome);
+        }catch(error) {
+            console.error("Erro na leitura do arquivo");
+        }
+    }
+})
